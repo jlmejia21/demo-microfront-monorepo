2038d565b29d5ae98ccbe98f71e6fc344bf054cd
Jose Luis Mejia Rojas <joseluismejiarojas@gmail.com>
2025-07-15 10:05:49 -0500

    feat: add environment

---
+++ projects/mf-shell/src/environments/environment.prd.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  remotes: {
+    mfShopping:
+      'https://gentle-glacier-0c894111e.2.azurestaticapps.net/remoteEntry.js',
+    mfPayment:
+      'https://happy-mushroom-0500dd21e.2.azurestaticapps.net/remoteEntry.js',
+  },
+};
---
+++ projects/mf-shell/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  remotes: {
+    mfShopping: 'http://localhost:4201/remoteEntry.js',
+    mfPayment: 'http://localhost:4202/remoteEntry.js',
+  },
+};
