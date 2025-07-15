0f9f47f639eb26db1fde7b6eaf6c622712137021
Jose Luis Mejia Rojas <joseluismejiarojas@gmail.com>
2025-07-15 10:03:23 -0500

    feat: load micro with loadRemoteModule and environment

--- projects/mf-shell/src/app/app.routes.ts
+++ projects/mf-shell/src/app/app.routes.ts
@@ -1,14 +1,24 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
+import { environment } from '../environments/environment';
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
-      import('mfShopping/ProductsComponent').then((m) => m.ProductsComponent),
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: environment.remotes.mfShopping,
+        exposedModule: './ProductsComponent',
+      }).then((m) => m.ProductsComponent),
   },
   {
     path: 'payment',
     loadComponent: () =>
-      import('mfPayment/PaymentComponent').then((m) => m.PaymentComponent),
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: environment.remotes.mfPayment,
+        exposedModule: './PaymentComponent',
+      }).then((m) => m.PaymentComponent),
   },
 ];
